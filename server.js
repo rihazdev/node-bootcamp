7dbe60c3f0962416cd43a623bc63f3f4eae0bfaa
Rihaz Zahir <rihazzahir@Rihazs-MacBook-Pro.local>
2023-09-05 16:17:51 +0530

    using express router

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const dotenv = require('dotenv');
 
+// Route files 
+const bootcamps = require('./routes/bootcamps');
+
 //Load env vars
 dotenv.config({path: './config/config.env'});
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+// Mount routers
+app.use('/api/v1/bootcamps', bootcamps);
 
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} node on port ${PORT}`));
