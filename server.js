07dbd18085ee8af96b6773d656d8b9af0d829294
Rihaz Zahir <rihazzahir@Rihazs-MacBook-Pro.local>
2023-09-06 11:28:33 +0530

    adding mmiddleware, MongoDB connection

--- server.js
+++ server.js
@@ -1,16 +1,32 @@
 const express = require('express');
 const dotenv = require('dotenv');
-
-// Route files 
-const bootcamps = require('./routes/bootcamps');
+const morgan = require('morgan');
+const connectDB = require('./config/db');
 
 //Load env vars
 dotenv.config({path: './config/config.env'});
 
-const app = express();
+// Route files 
+const bootcamps = require('./routes/bootcamps');
+
+// dev logging to console
+// const logger = require('./middleware/logger');
+
+const app = express(); 
 const PORT = process.env.PORT || 5000;
 
+// MongoDb connection
+connectDB();
+
+app.use(morgan('dev'));
+ 
 // Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} node on port ${PORT}`));
+const server = app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} node on port ${PORT}`));
+
+// handling unhandled promise rejection
+process.on('unhandledRejection', (err) => {
+    console.error(err);
+    server.close(() => process.exit(1));
+});
